4eb844ed6eeef0c4ab2ace4948714108d17e7f59
Tony Richards <linterra@gmail.com>
2016-04-16 08:38:06 -0400

    Adjusted client proxy for updated JSON api

--- src-server/proxy.js
+++ src-server/proxy.js
@@ -4,24 +4,26 @@ var debug = require('debug')('aether:registry');
 
 export default class ServiceRegistryProxy {
   constructor(config) {
-    this.server = config["service-registry"]["server"];
+    this.host = config["service-registry"]["host"];
     this.port = config["service-registry"]["port"];
-    this.request = requestJSON.createClient(`http://${this.server}:${this.port}/`);
+    this.request = requestJSON.createClient(`http://${this.host}:${this.port}/`);
   }
 
   register(path, address) {
     // Assume address is a fully formed URL including the protocol.
-    this.request.post(`/api/registry/register/${path}`, { address: address },
+    this.request.post(`/api/registry/${path}`, { address: address },
       (err, res, body) => {
-        console.log(err);
-        console.log(res);
-        console.log(body);
+        if (body.result == "success") {
+          debug("Registered with the service registry");
+        } else {
+          throw new Error(body.message);
+        }
       }
     );
   }
 
   get(path) {
-    this.request.get(`get/${path}`, (err, res, body) => {
+    this.request.get(`/api/registry/${path}`, (err, res, body) => {
       console.log(body);
     });
   }
